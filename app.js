7ac667451dcff0d2131b86e87ad019a7d3ab9a44
jonathanfmills <jonathanfmills@gmail.com>
2018-01-08 14:17:25 -0600

    4-0

--- app.js
+++ app.js
@@ -3,27 +3,12 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
-const sql = require('mssql');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const config = {
-  user: 'library',
-  password: 'psL1brary',
-  server: 'pslibrary.database.windows.net', // You can use 'localhost\\instance' to connect to named instance
-  database: 'PSLibrary',
-
-  options: {
-    encrypt: true // Use this if you're on Windows Azure
-  }
-};
-
-sql.connect(config).catch(err => debug(err));
-
 app.use(morgan('tiny'));
-
-
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bookRouter = express.Router();
-const sql = require('mssql');
-const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
   const books = [
@@ -55,39 +53,25 @@ function router(nav) {
     }];
   bookRouter.route('/')
     .get((req, res) => {
-      (async function query() {
-        const request = new sql.Request();
-        const { recordset } = await request.query('select * from books');
-        res.render(
-          'bookListView',
-          {
-            nav,
-            title: 'Library',
-            books: recordset
-          }
-        );
-      }());
+      res.render(
+        'bookListView',
+        {
+          nav,
+          title: 'Library',
+          books
+        }
+      );
     });
 
   bookRouter.route('/:id')
-    .all((req, res, next) => { 
-      (async function query() {
-        const { id } = req.params;
-        const request = new sql.Request();
-        const { recordset } =
-          await request.input('id', sql.Int, id)
-            .query('select * from books where id = @id');
-        [req.book] = recordset;
-        next();
-      }());
-    })
     .get((req, res) => {
+      const { id } = req.params;
       res.render(
         'bookView',
         {
           nav,
           title: 'Library',
-          book: req.book
+          book: books[id]
         }
       );
     });
