fa77030af508616e989d5de8fb3c1446bb48db79
jonathanfmills <jonathanfmills@gmail.com>
2018-01-10 15:15:34 -0600

    8-5

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,7 +1,28 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
+
+const parser = xml2js.Parser({ explicitArray: false });
+
+
 function goodreadsService() {
   function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'our description' });
+      axios.get('https://www.goodreads.com/book/show/656?key=r5oV4OPffoSbhrmou9GEGA')
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if(err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          console.log(error);
+        });
     });
   }
   return { getBookById };
